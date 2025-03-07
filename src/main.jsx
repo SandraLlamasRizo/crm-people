15724dcf5cdc7bdab3f3620421420f61575b02b4
Leticia <lberzal@gmail.com>
2025-03-07 13:08:38 +0100

    bootstrap eliminado

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter } from 'react-router'
 import MainRoutes from './routes.jsx'
 import Header from './components/Header.jsx'
