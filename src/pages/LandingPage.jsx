5bcd1f0af05c3a2d9870fa8b4ebc5168491ce712
Sandra <sandrallamasrizo@gmail.com>
2025-03-12 17:24:49 +0100

    arreglado landing page layout

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,9 +2,9 @@ import { NavLink } from "react-router";
 
 function LandingPage() {
     return (
-        <div className="h-screen flex flex-col items-center justify-center bg-[#F4F9FD] text-gray-700">
-            <header className="text-center px-6 flex-grow mt-10">
-                <img src="./Logo_CRM.svg" alt="" className="w-32 md:w-40 my-auto mx-auto" />
+        <div className="h-screen flex flex-col items-center align-middle justify-end bg-[#F4F9FD] text-gray-700">
+            <header className="text-center px-5 md:px-10 my-auto">
+                <img src="./Logo_CRM.svg" alt="" className="w-40 md:w-50 my-auto mx-auto" />
                 <h1 className="text-4xl md:text-5xl font-bold mb-4">
                     Gestiona tu negocio con facilidad
                 </h1>
@@ -20,8 +20,8 @@ function LandingPage() {
                     </NavLink>
                 </div>
             </header>
-            <div className="relative mt-4 bottom-0 w-full max-w-l md:max-w-2xl flex justify-center">
-                <img src="./LandingHero.svg" alt="CRM-HERO" className="w-full max-w-lg md:max-w-2xl  object-cover" />
+            <div className="relative bottom-0 w-full max-w-l md:max-w-4xl flex justify-center">
+                <img src="./LandingHero.svg" alt="CRM-HERO" className="w-full max-w-lg md:max-w-4xl  object-cover" />
             </div>
         </div>
     );
