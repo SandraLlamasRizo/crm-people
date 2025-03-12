2adde806b8c723a8dc944f2c736946e095398fbe
Sandra <sandrallamasrizo@gmail.com>
2025-03-12 13:40:27 +0100

    ajustes estilos employee cards

--- src/components/EmployeeCardSmall.jsx
+++ src/components/EmployeeCardSmall.jsx
@@ -3,7 +3,9 @@ function EmployeeCardSmall() {
     return <>
     <div className="flex gap-4 w-[174px] h-[180px]">
         <div className="p-3 text-center bg-[#F4F9FD] hover:bg-[#d3ebff] rounded-[30px]" >
-            <figure className="w-[50px] h-[50px] mx-auto p-2"><img className="rounded-full w-full h-full object-cover" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQK_mAcrV3vVhLq6HK4c1liqGV59qhOwXdEGw&s" alt="" /></figure>
+            <figure className="flex justify-center items-center">
+                <i className="bi bi-person-circle icon-large "></i>
+            </figure>
             <h3 className="principalCard pb-2">Nombre Empleado</h3>
             <h4 className="secundarioCard pb-3">Apellidos</h4>
             <h5 className="terciarioBorderCard inline-block p-1">Departamento</h5>
